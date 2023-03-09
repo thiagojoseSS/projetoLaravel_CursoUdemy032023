5a207eb459614268e6c0c376498815f9e4d514b7
ThiagoJoseSS <thiagoesoj@gmail.com>
2023-03-09 19:39:09 -0300

    routes studying

--- introducao/routes/web.php
+++ introducao/routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('users/{id}', function ($id) {
+    return "usuario e o seu id é $id";
+});
+
+Route::get('users', function () {
+    return "usuario";
+});
+
+Route::get('produtos/inserir', function () {
+    return "Página de Inserir produto";
+});
+
+// Route::get('produtos/{nome}', function ($nome) {
+//     return "produto é $nome";
+// });
+
+Route::get('produtos/{nome}/{valor?}', function ($nome, $valor = null) {
+    return "produto é $nome" . (($valor == null) ? "" : ", e o valor é $valor") . "!";
+});
+
