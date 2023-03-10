58c4a4fd8a24ee8c6612c8f0150cf568fa95d88f
ThiagoJoseSS <thiagoesoj@gmail.com>
2023-03-10 14:13:43 -0300

    Funções dentro do Controller

--- introducao/app/Http/Controllers/ProdutosController.php
+++ introducao/app/Http/Controllers/ProdutosController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class ProdutosController extends Controller
 {
-    //
+    public function index(){
+        return "Página de produto";
+    }
+
+    public function create(){
+        return "Página de Inserir produto";
+    }
+
+    public function show($nome, $valor = null){
+        return "produto é $nome" . (($valor == null) ? "" : ", e o valor é $valor") . "!";
+    }
 }
--- introducao/routes/web.php
+++ introducao/routes/web.php
@@ -17,7 +17,9 @@ use App\Http\Controllers\ProdutosController;
 
 Route::get('/', HomeController::class);
 
-Route::get('produtos/inserir', [ProdutosController::class, 'index']);
+Route::get('produtos/inserir', [ProdutosController::class, 'create']);
+Route::get('produtos/{nome}/{valor?}', [ProdutosController::class, 'show']);
+Route::get('produtos', [ProdutosController::class, 'index']);
 
 
 // Route::get('/', function () {
@@ -32,15 +34,15 @@ Route::get('users', function () {
     return "usuario";
 });
 
-Route::get('produtos/inserir', function () {
-    return "Página de Inserir produto";
-});
+// Route::get('produtos/inserir', function () {
+//     return "Página de Inserir produto";
+// });
 
 // Route::get('produtos/{nome}', function ($nome) {
 //     return "produto é $nome";
 // });
 
-Route::get('produtos/{nome}/{valor?}', function ($nome, $valor = null) {
-    return "produto é $nome" . (($valor == null) ? "" : ", e o valor é $valor") . "!";
-});
+// Route::get('produtos/{nome}/{valor?}', function ($nome, $valor = null) {
+//     return "produto é $nome" . (($valor == null) ? "" : ", e o valor é $valor") . "!";
+// });
 
