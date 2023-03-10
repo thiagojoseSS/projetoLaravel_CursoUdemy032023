b8614b2a1365a03e5e08bfe9be747bb02996f84a
ThiagoJoseSS <thiagoesoj@gmail.com>
2023-03-10 11:53:30 -0300

    Chamando Controller na Rota 1

--- introducao/routes/web.php
+++ introducao/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProdutosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +15,7 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', HomeController::class {
-    
-});
+Route::get('/', HomeController::class);
 
 // Route::get('/', function () {
 //     return view('welcome');
