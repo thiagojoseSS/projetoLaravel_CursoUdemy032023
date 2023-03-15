ce5541ec14699aefee8f7a7f8faa487c917dca87
ThiagoJoseSS <thiagoesoj@gmail.com>
2023-03-15 15:17:30 -0300

    Dominando BootStrap

--- introducao/resources/views/produtos/index.blade.php
+++ introducao/resources/views/produtos/index.blade.php
@@ -33,4 +33,41 @@
             {{$produtos->links()}}
         </div>
     </div>
+    <button class="btn btn-success position-relative" >
+        Inbox 
+        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+            99+
+            <span class="visually-hidden">unread messages</span>
+        </span>
+    </button>
+    <button class="btn btn-danger position-relative" >
+        Inbox 
+        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-warning">
+            99+
+            <span class="visually-hidden">unread messages</span>
+        </span>
+    </button>
+    <div class="btn-group" data-bs-toggle="buttons">
+        <label class="btn btn-light active">
+            <input type="radio" class="me-2" name="teeeeeeesssstes massa" id="teeeeeeesssstes massa" autocomplete="off" checked> Choice
+        </label>
+        <label class="btn btn-light">
+            <input type="radio" name="teeeeeeesssstes massa" id="teeeeeeesssstes massa" autocomplete="off"> Choice
+        </label>
+        <label class="btn btn-light">
+            <input type="radio" name="teeeeeeesssstes massa" id="teeeeeeesssstes massa" autocomplete="off"> Choice
+        </label>
+    </div>
+    <div class="btn-group" data-bs-toggle="buttons">
+        <label class="btn btn-danger active">
+            <input type="radio" class="me-2" name="vt" id="vt" autocomplete="off" checked> Café
+        </label>
+        <label class="btn btn-danger">
+            <input type="radio" name="vt" id="vt" autocomplete="off"> Chá
+        </label>
+        <label class="btn btn-danger">
+            <input type="radio" name="vt" id="vt" autocomplete="off"> Leite
+        </label>
+    </div>
+    
 @endsection
