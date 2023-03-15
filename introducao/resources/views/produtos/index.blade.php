373e734002ab8378b379f8272ee889f4b723cf0b
ThiagoJoseSS <thiagoesoj@gmail.com>
2023-03-15 15:02:12 -0300

    Ligando Bootstrap ao Projeto

--- introducao/resources/views/produtos/index.blade.php
+++ introducao/resources/views/produtos/index.blade.php
@@ -2,13 +2,35 @@
 @section('title', 'Página dos Produtos')
 @section('content')
     <h1 class="bg-black">Página dos Produtos</h1>
-    @foreach($produtos as $produto)
-    <p>
-        {{$produto->nome}}
-    </p>
-    @endforeach
+    <div class="w-2/3 container">
+        <table class="table-auto m-2 border-dashed border-2 border-orange-300 align-top">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Descrição</th>
+                    <th>Valor</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($produtos as $produto)
+                <tr>
+                    <td>
+                        {{$produto->nome}}
+                    </td>
+                    <td>
+                        {{$produto->descricao}}
+                    </td>
+                    <td>
+                        {{$produto->valor}}
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        
 
-    <div class="text-blue-800">
-        {{$produtos->links()}}
+        <div class="flex text-blue-800 px-8 origin-right transform skew-x-12">
+            {{$produtos->links()}}
+        </div>
     </div>
 @endsection
