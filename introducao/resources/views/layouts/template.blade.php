a8747998481357039a7f4d5f2a691d16aad7dfe4
ThiagoJoseSS <thiagoesoj@gmail.com>
2023-03-15 16:05:27 -0300

    NavBar e Buttons

--- introducao/resources/views/layouts/template.blade.php
+++ introducao/resources/views/layouts/template.blade.php
@@ -13,7 +13,7 @@
         crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
         integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD"
-        crossorigin="anonymous"></script>F
+        crossorigin="anonymous"></script>
     <style>
         th,
         td {
@@ -26,6 +26,37 @@
 </head>
 
 <body class="bg-clip-text text-blue-800 bg-gradient-to-t from-black to-gray-900">
+    <nav class="navbar navbar-expand-md navbar-light bg-light">
+          <div class="container">
+            <a class="navbar-brand" href="./">Navbar</a>
+            <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavId">
+                <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="produtos" aria-current="page">Produtos <span class="visually-hidden">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="users">Usuário</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownId">
+                            <a class="dropdown-item" href="#">Action 1</a>
+                            <a class="dropdown-item" href="#">Action 2</a>
+                        </div>
+                    </li>
+                </ul>
+                <form class="d-flex my-2 my-lg-0">
+                    <input class="form-control me-sm-2" type="text" placeholder="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+      </div>
+    </nav>
+    
     @yield('content')
 </body>
 
--- introducao/resources/views/produtos/index.blade.php
+++ introducao/resources/views/produtos/index.blade.php
@@ -1,73 +1,75 @@
 @extends('layouts.template')
 @section('title', 'Página dos Produtos')
 @section('content')
-    <h1 class="bg-black">Página dos Produtos</h1>
-    <div class="w-2/3 container">
-        <table class="table-auto m-2 border-dashed border-2 border-orange-300 align-top">
-            <thead>
-                <tr>
-                    <th>Nome</th>
-                    <th>Descrição</th>
-                    <th>Valor</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($produtos as $produto)
-                <tr>
-                    <td>
-                        {{$produto->nome}}
-                    </td>
-                    <td>
-                        {{$produto->descricao}}
-                    </td>
-                    <td>
-                        {{$produto->valor}}
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-        
+<div class="w-2/3 container">
+    <a name="inserirProduto" id="inserirProduto" class="btn btn-success mt-4 mb-4" href="produtos/inserir" role="button">Inserir
+        Produto</a>
+    <table class="table-auto border-dashed border-2 border-orange-300 align-top">
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Descrição</th>
+                <th>Valor</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($produtos as $produto)
+            <tr>
+                <td>
+                    {{$produto->nome}}
+                </td>
+                <td>
+                    {{$produto->descricao}}
+                </td>
+                <td>
+                    {{$produto->valor}}
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 
-        <div class="flex text-blue-800 px-8 origin-right transform skew-x-12">
-            {{$produtos->links()}}
-        </div>
+
+    <div class="flex text-blue-800 px-8 origin-right transform skew-x-12">
+        {{$produtos->links()}}
     </div>
-    <button class="btn btn-success position-relative" >
-        Inbox 
-        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-            99+
-            <span class="visually-hidden">unread messages</span>
-        </span>
-    </button>
-    <button class="btn btn-danger position-relative" >
-        Inbox 
-        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-warning">
-            99+
-            <span class="visually-hidden">unread messages</span>
-        </span>
-    </button>
-    <div class="btn-group" data-bs-toggle="buttons">
-        <label class="btn btn-light active">
-            <input type="radio" class="me-2" name="teeeeeeesssstes massa" id="teeeeeeesssstes massa" autocomplete="off" checked> Choice
-        </label>
-        <label class="btn btn-light">
-            <input type="radio" name="teeeeeeesssstes massa" id="teeeeeeesssstes massa" autocomplete="off"> Choice
-        </label>
-        <label class="btn btn-light">
-            <input type="radio" name="teeeeeeesssstes massa" id="teeeeeeesssstes massa" autocomplete="off"> Choice
-        </label>
-    </div>
-    <div class="btn-group" data-bs-toggle="buttons">
-        <label class="btn btn-danger active">
-            <input type="radio" class="me-2" name="vt" id="vt" autocomplete="off" checked> Café
-        </label>
-        <label class="btn btn-danger">
-            <input type="radio" name="vt" id="vt" autocomplete="off"> Chá
-        </label>
-        <label class="btn btn-danger">
-            <input type="radio" name="vt" id="vt" autocomplete="off"> Leite
-        </label>
-    </div>
-    
+</div>
+<button class="btn btn-success position-relative m-2">
+    Inbox
+    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+        99+
+        <span class="visually-hidden">unread messages</span>
+    </span>
+</button>
+<button class="btn btn-danger position-relative m-2">
+    Inbox
+    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-warning">
+        99+
+        <span class="visually-hidden">unread messages</span>
+    </span>
+</button>
+<div class="btn-group m-2" data-bs-toggle="buttons">
+    <label class="btn btn-light active">
+        <input type="radio" class="me-2" name="teeeeeeesssstes massa" id="teeeeeeesssstes massa" autocomplete="off"
+            checked> Vitor
+    </label>
+    <label class="btn btn-light">
+        <input type="radio" name="teeeeeeesssstes massa" id="teeeeeeesssstes massa" autocomplete="off"> Alison
+    </label>
+    <label class="btn btn-light">
+        <input type="radio" name="teeeeeeesssstes massa" id="teeeeeeesssstes massa" autocomplete="off"> Thiago
+    </label>
+</div>
+<div class="btn-group m-2" data-bs-toggle="buttons">
+    <label class="btn btn-danger active">
+        <input type="radio" class="me-2" name="vt" id="vt" autocomplete="off" checked> Café
+    </label>
+    <label class="btn btn-danger">
+        <input type="radio" name="vt" id="vt" autocomplete="off"> Chá
+    </label>
+    <label class="btn btn-danger">
+        <input type="radio" name="vt" id="vt" autocomplete="off"> Leite
+    </label>
+</div>
+
 @endsection
